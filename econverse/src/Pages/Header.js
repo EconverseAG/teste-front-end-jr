32f7cf6bbe358a087d1b3fe13ed54457286036b3
PriscillaLucena <prilucenaoliveira@gmail.com>
2023-03-07 20:22:20 -0300

    debbugado header

--- econverse/src/Pages/Header.js
+++ econverse/src/Pages/Header.js
@@ -1,10 +1,6 @@
 import React from "react";
 import '../Styles/HeaderLayout.css';
-import logo from "../Images/logo.png";
-import v from "../Images/v.png";
-import t from "../Images/t.png";
-import e from "../Images/e.png";
-import x from "../Images/x.png";
+import logo from "../Images/vtex-logo-rosa.svg";
 import caixa from "../Images/caixaCompra.png";
 import coracao from "../Images/Heart.png";
 import perfil from "../Images/UserCircle.png";
@@ -12,7 +8,7 @@ import carrinho from "../Images/ShoppingCart.png";
 import shield from "../Images/ShieldCheck.svg";
 import frete from "../Images/Truck.svg";
 import cartao from "../Images/CreditCard.svg";
-import assinatura from "../Images/CrownSimple.svg";
+import coroa from "../Images/CrownSimple.svg";
 import desconto from "../Images/Rectangle250.png";
 
 export const Header = () => {
@@ -22,7 +18,7 @@ export const Header = () => {
 
                 <img className="vetor-shield" src={shield} />
                 <div className="posiciona-frase-shield">
-                    <p className="letras-cinza">Compra <span className="letras-rosa"> 100% segura</span></p>
+                    <span className="letras-cinza">Compra </span><span className="letras-rosa"> 100% segura</span>
                 </div>
 
                 <img className="vetor-frete" src={frete} />
@@ -38,31 +34,31 @@ export const Header = () => {
             </div>
             <div className="container-input-vetor ">
                 <img className="vetor-vtex " src={logo} />
-                <img className="vetor-v" src={v} />
-                <img className="vetor-t" src={t} />
-                <img className="vetor-e" src={e} />
-                <img className="vetor-x" src={x} />
+
                 <input id="input1" placeholder="O que você está buscando?" />
                 <img className="vetor-caixa" src={caixa} />
                 <img className="vetor-coracao" src={coracao} />
                 <img className="vetor-perfil" src={perfil} />
                 <img className="vetor-carrinho" src={carrinho} />
             </div>
-            <div className="container-categorias">
-                <h4 className="todas-categorias">todas as categorias</h4>
-                <h4 className="suhpermercado">supermercado</h4>
-                <h4 className="livros">livros</h4>
-                <h4 className="moda">moda</h4>
-                <h4 className="lancamentos">lançamentos</h4>
-                <h4 className="ofertas-do-dia">ofertas do dia</h4>
-                <div className="container-assinatura">
-                    <img src={assinatura} />
-                    <h4>assinatura</h4>
+
+            <div className="container-geral-categorias">
+                <div className="container-categorias">
+                    <span className="todas-categorias">todas as categorias</span>
+                    <span className="supermercado">supermercado</span>
+                    <span className="livros">livros</span>
+                    <span className="moda">moda</span>
+                    <span className="lancamentos">lançamentos</span>
+                    <span className="ofertas-do-dia">ofertas do dia</span>
+                    <div className="container-assinatura">
+                        <img id="coroa" src={coroa}/>
+                        <span id="assinatura">assinatura</span>
+                    </div>
                 </div>
             </div>
             <div className="container-descontos">
                 <div className="container-imagem-descontos">
-                <img className="imagem-desconto" src={desconto}/>
+                    <img className="imagem-desconto" src={desconto} />
                 </div>
                 <p className="paragrafo-desconto">Venha conhecer nossas promoções</p>
                 <p className="paragrafo-desconto-2">50% off nos produtos</p>
