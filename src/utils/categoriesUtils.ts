0dbda2d999911b17bdbe15ee7209278c6c224b0c
kayky <kayky140703@gmail.com>
2023-09-12 14:56:06 -0300

    refactor: add home component

---
+++ src/utils/categoriesUtils.ts
@@ -0,0 +1,48 @@
+const categories = [
+    {
+      isFirstCategory: true,
+      urlImage: '/tabletAndSmartphone.svg',
+      name: 'Tecnologia',
+      id: 1
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/supermercados.svg',
+      name: 'Supermercado',
+      id: 2
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/whiskey.svg',
+      name: 'Bebidas',
+      id: 3
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/ferramentas.svg',
+      name: 'Ferramentas',
+      id: 4
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/cuidadosDeSaude.svg',
+      name: 'Saúde',
+      id: 5
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/corrida.svg',
+      name: 'Esportes e Fitness',
+      id: 6
+    },
+    {
+      isFirstCategory: false,
+      urlImage: '/moda.svg',
+      name: 'Moda',
+      id: 7
+    },
+  
+  ]
+  
+  
+  export default categories
