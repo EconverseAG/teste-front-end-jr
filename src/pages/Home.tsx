cbcbb8d10d4e44c31066bd51eb69fae1ad1e5425
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-09-15 16:32:57 -0300

    feat: adicionado o componente home.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+import Main from "../components/Main/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
