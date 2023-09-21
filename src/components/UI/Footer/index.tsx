f5dd9513997c8657ae005637d0846572f922ab4c
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-20 23:39:59 -0300

    feat: create routing for brands section and almost finishing the footer component

---
+++ src/components/UI/Footer/index.tsx
@@ -0,0 +1,79 @@
+import './styles.scss';
+
+import econverseLogoIcon from '../../../assets/econverse-logo.svg';
+import vtexLogoIcon from '../../../assets/vtex.svg';
+
+import facebookIcon from '../../../assets/icons/facebook.svg';
+import instagramIcon from '../../../assets/icons/instagram.svg';
+import youtubeIcon from '../../../assets/icons/youtube.svg';
+import paymentFlags from '../../../assets/payment-flags.svg';
+
+export function Footer() {
+  return (
+    <footer className="footer-container">
+
+      <div className="top">
+        <div className="options">
+          <nav className="about-us-section">
+            <h4>Sobre nós</h4>
+
+            <div className="links-container">
+              <a href="#">Conheça</a>
+              <a href="#">Como chegar</a>
+              <a href="#">Indicação e desconto</a>
+            </div>
+
+            <div className="social-container">
+              <a href="#" title="Vtrex Facebook">
+                <img src={facebookIcon} alt="Ícone do Facebook" />
+              </a>
+              <a href="#" title="Vtrex Instagram">
+                <img src={instagramIcon} alt="Ícone do Instagram" />
+              </a>
+              <a href="#" title="Vtrex Youtube">
+                <img src={youtubeIcon} alt="Ícone do Youtube" />
+              </a>
+            </div>
+          </nav>
+          <nav className="info-section">
+            <h4>Informações úteis</h4>
+
+            <div className="links-container">
+              <a href="#">Fale conosco</a>
+              <a href="#">Dúvidas</a>
+              <a href="#">Prazos de entrega</a>
+              <a href="#">Formas de Pagamento</a>
+              <a href="#">Política de privacidade</a>
+              <a href="#">Trocas e devoluções</a>
+            </div>
+          </nav>
+          <nav className="payments-section">
+            <h4>Formas de pagamento</h4>
+
+            <div className="payment-flags">
+              <img src={paymentFlags} alt="Todas as bandeiras disponiveis para pagamento" />
+            </div>
+          </nav>
+        </div>
+        <div className="email-container">
+          <h3>Cadastre-se e Receba nossas <strong>novidades e promoções</strong></h3>
+          <form>
+
+          </form>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <span>
+          Copyright © 2019. Todos os direitos reservados. Todas as marcas e suas imagens são de propriedade de seus respectivos donos.
+          É vedada a reprodução, total ou parcial, de qualquer conteúdo sem expressa autorização.
+        </span>
+
+        <div className="brands">
+          <img src={econverseLogoIcon} alt="econverse logo" />
+          <img src={vtexLogoIcon} alt="VTEX logo" />
+        </div>
+      </div>
+    </footer>
+  )
+}
