8cb5309473a4583de136b7b0b7ba6fbef16d8935
Vitor Ruis <108823068+vitorwhois@users.noreply.github.com>
2024-06-26 21:29:50 -0300

    chore: create project structure with components, styles, and utilities folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/global.scss';
+
 
 function App() {
   const [count, setCount] = useState(0)
