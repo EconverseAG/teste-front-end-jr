9f5c4ff4a270ba063fe79d77e92ba8fcb7ddd492
lucas <lucasroque545@gmail.com>
2025-01-20 00:14:14 -0300

    Update Vite configuration: Add React plugin and remove proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://app.econverse.com.br',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
