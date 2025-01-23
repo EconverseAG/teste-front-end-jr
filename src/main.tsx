0c014392b1de799d6203327ef2ff16a45da129a3
felipedestroo <fdestrocontact@gmail.com>
2025-01-23 17:09:05 -0300

    feat: add hero section

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Navbar } from './components/navbar'
-
+import { Hero } from './components/hero'
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar />
+    <Hero />
   </StrictMode>,
 )
